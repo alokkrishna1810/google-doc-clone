97109061d4e0a3d7190e003635902bdff7f99522
alokkrishna1810 <alokkrishna456@gmail.com>
2025-08-26 04:48:59 +0530

    created a demo document redirecting link

---
+++ src/app/documents/[documentId]/page.tsx
@@ -0,0 +1,9 @@
+interface DocumentIdPageProps {
+  params: Promise<{ documentId: string }>;
+}
+
+export default async function DocumentIdPage({ params }: DocumentIdPageProps) {
+  const { documentId } = await params;
+
+  return <div className="min-h-screen bg-[#FAFBFD]">Document Id: {documentId}</div>;
+}
